4a71d35b5576b5155a1e063acb5d03dd31bcd9dc
Erfan Dousti <erfan.flashpoint@gmail.com>
2026-08-04 15:55:25 -0400

    Even thoguh it's o(n) space, it's easier to understan

--- LeetCode/easy/arrays/shuffle-the-array/description/solution.py
+++ LeetCode/easy/arrays/shuffle-the-array/description/solution.py
@@ -1,9 +1,7 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        ans = [0] * (2 * n)
-        for i in range(2 * n):
-            if i % 2 == 0:
-                ans[i] = nums[i // 2]
-            else:
-                ans[i] = nums[n + i // 2]
-        return ans
+        res = []
+        for i in range(len(nums)):
+            res.append(nums[i])
+            res.append(nums[i + n])
+        return res
