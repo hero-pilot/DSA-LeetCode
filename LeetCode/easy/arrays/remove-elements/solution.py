f92985c9b3dc626787270e3addffe0eb8977b160
Erfan Dousti <erfan.flashpoint@gmail.com>
2026-08-26 13:36:24 -0400

    This one is easier to understand

--- LeetCode/easy/arrays/remove-elements/solution.py
+++ LeetCode/easy/arrays/remove-elements/solution.py
@@ -1,8 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        index = 0
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[index] = nums[i]
-                index += 1
-        return index
+        n = len(nums) 
+        i =0 
+        while i < n :
+            if nums[i] == val:
+                nums[i] = nums[n - 1]
+                n -=1
+            else:
+                i +=1
+        return n
